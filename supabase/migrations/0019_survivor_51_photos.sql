9d9d259916298532cefab43aac4123c77a3400f5
Jim Elowitt <elowittjim@gmail.com>
2026-08-29 14:57:38 -0700

    Add Survivor 51 castaway headshots
    21 face-cropped headshots (400x400) sourced from People.com's cast
    reveal photos, using each photo's editorial focal point to center the
    crop tightly on the face. Migration 0019 points each contestant's
    photo_url at their new file.

---
+++ supabase/migrations/0019_survivor_51_photos.sql
@@ -0,0 +1,25 @@
+-- Survivor 51 castaway headshots, cropped from People.com's cast reveal
+-- photos and served as static files from /public/contestants/ (same
+-- pattern as 0009). Not using the lower(name)-concatenation shortcut 0009
+-- used, since "Thien An" has a space that doesn't map cleanly to a filename.
+update public.contestants set photo_url = '/contestants/rob.jpg' where name = 'Rob';
+update public.contestants set photo_url = '/contestants/brady.jpg' where name = 'Brady';
+update public.contestants set photo_url = '/contestants/patt.jpg' where name = 'Patt';
+update public.contestants set photo_url = '/contestants/linnea.jpg' where name = 'Linnea';
+update public.contestants set photo_url = '/contestants/cristian.jpg' where name = 'Cristian';
+update public.contestants set photo_url = '/contestants/sharonda.jpg' where name = 'Sharonda';
+update public.contestants set photo_url = '/contestants/jenna.jpg' where name = 'Jenna';
+update public.contestants set photo_url = '/contestants/kristin.jpg' where name = 'Kristin';
+update public.contestants set photo_url = '/contestants/ori.jpg' where name = 'Ori';
+update public.contestants set photo_url = '/contestants/lewis.jpg' where name = 'Lewis';
+update public.contestants set photo_url = '/contestants/kilby.jpg' where name = 'Kilby';
+update public.contestants set photo_url = '/contestants/carter.jpg' where name = 'Carter';
+update public.contestants set photo_url = '/contestants/alexis.jpg' where name = 'Alexis';
+update public.contestants set photo_url = '/contestants/jelly.jpg' where name = 'Jelly';
+update public.contestants set photo_url = '/contestants/eric.jpg' where name = 'Eric';
+update public.contestants set photo_url = '/contestants/maggie.jpg' where name = 'Maggie';
+update public.contestants set photo_url = '/contestants/thienan.jpg' where name = 'Thien An';
+update public.contestants set photo_url = '/contestants/mike.jpg' where name = 'Mike';
+update public.contestants set photo_url = '/contestants/aaliyah.jpg' where name = 'Aaliyah';
+update public.contestants set photo_url = '/contestants/ana.jpg' where name = 'Ana';
+update public.contestants set photo_url = '/contestants/devin.jpg' where name = 'Devin';
